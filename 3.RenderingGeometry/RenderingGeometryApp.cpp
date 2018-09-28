4b7f5b838d229ab50fc28ad77231f30c2de735fd
Ralenski Doucet <Ralenskid@gmail.com>
2018-09-28 12:48:40 -0500

    commit
    need to test

--- 3.RenderingGeometry/RenderingGeometryApp.cpp
+++ 3.RenderingGeometry/RenderingGeometryApp.cpp
@@ -40,33 +40,100 @@ void RenderingGeometryApp::draw()
 
 }
 
-std::vector<glm::vec3> RenderingGeometryApp::genHalfCircle(int np, double radius)
+std::vector<glm::vec4> RenderingGeometryApp::genHalfCircle(int np, double radius)
 {
 	//1st two arguments int np(Number of Points); double radius;
 	//2nd declare number of points;
 	//3rd declare local varaible that will represent an vertex's position.
-
+	std::vector<glm::vec4>CircleVerts;
 	
-	
-
-	for (int theta = 3.14;  theta = radius / np ; radius =180)
+	for (float i=0;i<np;i++)
 	{
-		
-	
 		//calculate (angle or theta) in for loop.
 		//angle is equals the answer of (3.14/number of points)
-		
+		float angle = glm::pi<float>() / ((float) np - 1);
+		float theta = i * angle;
+	
 		//push back each vertice in the vertex _points->
-		//that shows each generated portion of the half circle.
-		
+		//that shows each generated portion of the half circle
+		CircleVerts.push_back(glm::vec4(glm::cos(theta)*radius, glm::sin(theta)*radius, 0, 1));
 		
 	}
-	CircleVerts.push_back();
+	return CircleVerts;
+	
 }
 
-std::vector<glm::vec3> RenderingGeometryApp::genSphere(int np, int mer)
+std::vector<glm::vec4> RenderingGeometryApp::genSphere(std::vector<glm::vec4>points, unsigned int numofM)
 {
-//	std::vector<glm::vec4> allPoints;
 
+	std::vector<glm::vec4> SpherePoints;   
+	for (int i = 0; i < numofM + 1; i++) 
+	{
+		float sphereSlice = (glm::pi<float>() * 2) / (float)numofM; 
+		float theta = i * sphereSlice;  
+		for (int j = 0; j < points.size(); j++) 
+		{ 
+			float X = points[j].x;  
+			float Y = points[j].y * cos(theta) + points[j].z * -sin(theta);        
+			float Z = points[j].z * cos(theta) + points[j].y * sin(theta);      
+			glm::vec4 point = glm::vec4(X, Y, Z, 1); 
+			SpherePoints.push_back(point);
+		} 
+	}   
+	return SpherePoints;
+
+}
+
+std::vector<unsigned int> RenderingGeometryApp::genSphereIndices(unsigned int np, unsigned int numM)
+{
+	std::vector<unsigned int> Sphereindices;  
+	unsigned int start;   
+	unsigned int bottom_left;  
+	unsigned int bottom_right;   
+	for (int r = 0; r < numM; r++) 
+	{
+		start = y * np; 
+		for (int p = 0; p < np; p++) 
+		{
+			bottom_left = start + p;
+			bottom_right = bottom_left + np;
+			Sphereindices.push_back(bottom_left);
+			Sphereindices.push_back(bottom_right); 
+		}     
+		Sphereindices.push_back(0xFFFF);
+	} 
+	return Sphereindices;
+
+}
+
+std::vector<Vertex> RenderingGeometryApp::genPlane(int size)
+{
+	Vertex A = Vertex(glm::vec4(-size, size, 0, 1), glm::vec4(1, 0, 0, 1));  
+	Vertex B = Vertex(glm::vec4(size, size, 0, 1), glm::vec4(1, 0, 0, 1));
+	Vertex C = Vertex(glm::vec4(size, -size, 0, 1), glm::vec4(1, 0, 0, 1)); 
+	Vertex D = Vertex(glm::vec4(-size, -size, 0, 1), glm::vec4(1, 0, 0, 1));  
+	std::vector<Vertex> PlaneVertices = { A,B,C,D };   
+	return PlaneVertices;
+}
+
+std::vector<Vertex> RenderingGeometryApp::genCube(std::vector<Vertex> vertices)
+{
+	std::vector<Vertex> CubePoints;    
+	 
+	CubePoints.push_back(Vertex(glm::vec4(0, 1, 1, 1),glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(1, 1, 1, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(1, 0, 1, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(0, 0, 1, 1), glm::vec4(1)));   
+	CubePoints.push_back(Vertex(glm::vec4(0, 0, 0, 1), glm::vec4(1))); 
+	CubePoints.push_back(Vertex(glm::vec4(1, 0, 0, 1), glm::vec4(1))); 
+	CubePoints.push_back(Vertex(glm::vec4(1, 1, 0, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(0, 1, 0, 1), glm::vec4(1)));   
+	CubePoints.push_back(Vertex(glm::vec4(0, 1, 1, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(1, 1, 1, 1), glm::vec4(1)));  
+	CubePoints.push_back(Vertex(glm::vec4(1, 1, 0, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(1, 0, 0, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(0, 1, 0, 1), glm::vec4(1)));
+	CubePoints.push_back(Vertex(glm::vec4(0, 0, 0, 1), glm::vec4(1)));
+	return CubePoints;
 }
 
--- 3.RenderingGeometry/RenderingGeometryApp.h
+++ 3.RenderingGeometry/RenderingGeometryApp.h
@@ -1,4 +1,5 @@
 #pragma once
+#define GLM_FORCE_SWIZZLE
 #include "Shader.h"
 #include "MeshRenderer.h"
 #include <Transform.h>
@@ -17,15 +18,12 @@ public:
 	void shutdown() override;
 	void update(float dt) override;
 	void draw() override;
-	std::vector <glm::vec3> CircleVerts =
-	{ CircleVerts[1] = { 1, 0, 0 },
-		CircleVerts[2] = { .50,.50,0 },
-		CircleVerts[3] = { 1,1,0 },
-		CircleVerts[4] = { -.50,50,0 },
-		CircleVerts[5] = { -1,0,0 },
-	};
-	std::vector <glm::vec3> genHalfCircle(int np, double radius);
-	std::vector<glm::vec3> genSphere(int np, int mer);
+	
+	std::vector <glm::vec4> genHalfCircle(int np, double radius);
+	std::vector<glm::vec4> genSphere(std::vector<glm::vec4>points, unsigned int numofM);
+	std::vector<unsigned int>genSphereIndices(unsigned int np, unsigned int numM);
+	std::vector<Vertex> genPlane(int size);
+	std::vector<Vertex>genCube(std::vector<Vertex> vertices)
 private:
 	Shader *mShader;
 	MeshRenderer* mMesh;
