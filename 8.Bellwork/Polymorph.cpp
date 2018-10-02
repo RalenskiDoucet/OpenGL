0240c3463683f6d714dce51b9b8b36754b617342
Ralenski Doucet <Ralenskid@gmail.com>
2018-10-02 10:57:33 -0500

    added a project for all bellwork assignments

---
+++ 8.Bellwork/Polymorph.cpp
@@ -0,0 +1,18 @@
+//#include "Polymorph.h"
+//
+//Polymorph::Polymorph()
+//{
+//
+//}
+//
+//int Polymorph::changeShape(int shape)
+//{
+//	int triangle;
+//	int square;
+//	int circle;
+//	int currentShape = triangle;
+//	shape = 0;
+//	int 
+//	for ( > 0;)
+}
+
---
+++ 8.Bellwork/Polymorph.h
@@ -0,0 +1,11 @@
+#pragma once
+class Polymorph
+{
+public:
+	Polymorph();
+
+	float x_Pos;
+	float y_Pos;
+	int changeShape(int shape);
+};
+
